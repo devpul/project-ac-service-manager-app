3bcd1f702f217756347c1ea25ad4367757cc5f08
ibnubachdar <ibnubachdar@gmail.com>
2025-12-19 19:56:37 +0700

    feat: add toko migration table

--- database/migrations/2025_12_19_192213_create_tokos_table.php
+++ database/migrations/2025_12_19_192213_create_tokos_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('tokos', function (Blueprint $table) {
             $table->id();
+            $table->integer('site')->nullable();
+            $table->string('site_descr')->nullable();
+            $table->string('store_type')->nullable();
+            $table->date('go_date')->nullable();
+            $table->string('province')->nullable();
+            $table->string('city')->nullable();
+            $table->string('district')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
