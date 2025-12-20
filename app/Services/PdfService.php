faf64ed7087989391fa1d5ed142860cf7175ef5c
ibnubachdar <ibnubachdar@gmail.com>
2025-12-20 12:13:36 +0700

    refactor: download pdf

--- app/Services/PdfService.php
+++ app/Services/PdfService.php
@@ -7,20 +7,6 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class PdfService
 {
-    // public function download(string $html, string $filename)
-    // {
-    //     return Pdf::loadHTML($html)
-    //             ->setPaper('A4', 'portrait')
-    //             ->download($filename);
-    // }
-
-    // public function stream(string $html, string $filename)
-    // {
-    //     return Pdf::loadHTML($html)
-    //             ->setPaper('A4', 'portrait')
-    //             ->stream($filename);
-    // }
-
     public static function download(string $html, string $filename)
     {
         return Pdf::loadHTML($html)
